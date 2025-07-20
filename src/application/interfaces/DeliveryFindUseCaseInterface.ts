6cd11b42ee49f53599558fd4dc5856c147a384b5
Carles Torrubiano <ctorrubiano@dogfydiet.com>
2025-07-20 10:40:38 +0200

    Rename id for trackingNumber

--- src/application/interfaces/DeliveryFindUseCaseInterface.ts
+++ src/application/interfaces/DeliveryFindUseCaseInterface.ts
@@ -2,5 +2,5 @@ import { DeliveryFindResponseDTO } from "../dtos/DeliveryFindDTO"
 
 export interface DeliveryFindUseCaseInterface {
   findById(id: string): Promise<DeliveryFindResponseDTO>
-  findByTrackingNumber(id: string): Promise<DeliveryFindResponseDTO>
+  findByTrackingNumber(trackingNumber: string): Promise<DeliveryFindResponseDTO>
 }
